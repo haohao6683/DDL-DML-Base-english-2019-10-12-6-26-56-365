7e481d63a1957cf5dd1adc204cbdbad0c84f5e42
haohao6683 <51568477+haohao6683@users.noreply.github.com>
2020-07-03 17:46:55 +0800

    Update dml.sql

--- dml.sql
+++ dml.sql
@@ -1,7 +1,12 @@
 -- Insert record
-
+INSERT INTO tableName ( field1, field2,...fieldN )
+                       VALUES
+                       ( value1, value2,...valueN );
 -- Revise record
-
+UPDATE tableName SET field1=new-value1, field2=new-value2;
 --  Delete record
-
+DELETE FROM tableName [WHERE Clause];
 -- Search record
+SELECT column_name,column_name
+ FROM table_name
+  [WHERE Clause];
